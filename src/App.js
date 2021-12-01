b92a22727ac9f0651a22a679cdfa484ee7ca0305
awmills25 <awmills25@gmail.com>
2021-11-30 20:34:59 -0500

    setting up blog editor page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import MyNavbar from "./Components/MyNavbar";
 import Footer from "./Components/Footer";
 import Home from "../src/Pages/Home";
+import Editor from "../src/Pages/Editor";
 import Lineup from "../src/Pages/Lineup";
 import Standings from "./Pages/Standings";
 import Schedule from "./Pages/Schedule";
@@ -10,6 +11,8 @@ import History from "../src/Pages/History";
 import Rules from "../src/Pages/Rules";
 import NewUser from "../src/Pages/NewUser";
 import Login from "../src/Pages/Login";
+
+const user = { membership: "Commissioner" };
 
 function App() {
   return (
@@ -24,7 +27,10 @@ function App() {
           <Route path="/rules" element={<Rules />} />
           <Route path="/newuser" element={<NewUser />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/*" element={<Home />} />
+          {user.membership === "Commissioner" && (
+            <Route path="/editor" element={<Editor />} />
+          )}
+          <Route path="/" element={<Home />} />
         </Routes>
       </main>
       <Footer />
---
+++ src/Pages/Editor.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from "react";
+import { Form, Container, Row, Col, Button } from "react-bootstrap";
+
+export default function Editor() {
+  const [content, setContent] = useState("");
+  const [updateStatus, setUpdateStatus] = useState("idle");
+  const [fetchStatus, setFetchStatus] = useState("idle");
+
+  useEffect(() => {
+    async function fetchCurrentBlogContent() {
+      setFetchStatus("loading");
+      //axios call
+      //to do -- fetch current blog content on load
+      //to do -- set default content state to current blog content
+      //if successful fetch, setFetchStatus("success");
+    }
+    fetchCurrentBlogContent();
+  }, []);
+
+  function handleChange(event) {
+    setContent(event.target.value);
+  }
+
+  async function submit(event) {
+    event.preventDefault();
+    setUpdateStatus("loading");
+    //axios post to update the blog content
+    //if successful, navigate to home page
+    console.log(content);
+  }
+
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <Form onSubmit={submit} disabled={updateStatus === "loading"}>
+            <Form.Group
+              className="mb-3"
+              controlId="exampleForm.ControlTextarea1"
+            >
+              <Form.Label>Write new blog post below</Form.Label>
+              <Form.Control
+                as="textarea"
+                rows={3}
+                value={content}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            <Button variant="outline-dark" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
