cb72c2bad784fce1eb82267f713aac87a476eef2
awmills25 <awmills25@gmail.com>
2021-12-01 12:00:30 -0500

    setting up login

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import MyNavbar from "./Components/MyNavbar";
 import Footer from "./Components/Footer";
@@ -14,20 +14,51 @@ import Login from "../src/Pages/Login";
 
 function App() {
   const user = { membership: "Commissioner" }; //use api to determine membership status later on
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    let myToken = window.localStorage.getItem("token");
+    if (myToken) {
+      setToken(myToken);
+    }
+  });
+
+  const saveToken = (userToken) => {
+    localStorage.setItem("token");
+    setToken(userToken);
+  };
+
+  const removeToken = () => {
+    localStorage.removeItem("token");
+    setToken("");
+  };
+
   return (
     <>
-      <MyNavbar />
+      <MyNavbar removeToken={removeToken} token={token} />
       <main>
         <Routes>
-          <Route path="/lineup" element={<Lineup />} />
+          <Route
+            path="/lineup"
+            element={<Lineup token={token} saveToken={saveToken} />}
+          />
           <Route path="/standings" element={<Standings />} />
           <Route path="/schedule" element={<Schedule />} />
           <Route path="/history" element={<History />} />
           <Route path="/rules" element={<Rules />} />
-          <Route path="/newuser" element={<NewUser />} />
-          <Route path="/login" element={<Login />} />
+          <Route
+            path="/newuser"
+            element={<NewUser token={token} saveToken={saveToken} />}
+          />
+          <Route
+            path="/login"
+            element={<Login token={token} saveToken={saveToken} />}
+          />
           <Route path="/editor" element={<Editor user={user} />} />
-          <Route path="/*" element={<Home />} />
+          <Route
+            path="/*"
+            element={<Home token={token} saveToken={saveToken} />}
+          />
         </Routes>
       </main>
       <Footer />
--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import { Navigate } from "react-router-dom";
 import axios from "axios";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
 
-export default function Login() {
-  let token = "";
-  function handleClick(event) {
+export default function Login(props) {
+  const [login, setLogin] = useState({});
+
+  const getLogin = (event) => {
+    event.preventDefault();
     axios({
       method: "post",
       url: "https://laravel-awmills25552543.codeanyapp.com/oauth/token",
       data: {
-        //email
-        //password
+        username: login.email,
+        password: login.password,
+        grant_type: "password",
+        client_id: "2",
+        client_secret: "4omddLSCpbDQb0nVxdcB8qzen8DRav4BVV71zuup",
+        scope: "",
       },
       headers: {
         Accept: "application/json",
@@ -19,22 +26,28 @@ export default function Login() {
         "Access-Control-Allow-Headers": "Content-Type",
         "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
         "Access-Control-Allow-Credentials": true,
-        Authorization: `Bearer ${token}`,
+        // Authorization: `Bearer ${token}`,
       },
-    }).then(function (response) {
-      console.log(response);
-      token.saveToken(response.data.data.token);
-    });
-  }
+    })
+      .then(function (response) {
+        console.log(response);
+        props.saveToken(response.data.access_token);
+      })
+      .catch(function (error) {
+        console.log({ error });
+      });
+  };
 
-  return (
+  return props.token.length > 0 ? (
+    <Navigate to="/lineup" />
+  ) : (
     <>
       <Container className="text-center">
         <Row className="justify-content-center">
           <Col className="col-4">
             <h1 className="text-center">LOGIN</h1>
             <hr></hr>
-            <Form>
+            <Form onSubmit={getLogin}>
               <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Email address</Form.Label>
                 <Form.Control type="email" placeholder="Enter email" />
